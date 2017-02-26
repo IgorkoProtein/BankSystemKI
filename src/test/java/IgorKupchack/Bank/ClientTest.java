c780f09cbcd879fb10be8faa55e394a8d011fb73
NAzarMh <nazar.myhailiv@gmail.com>
2017-02-26 11:48:06 +0200

    Task #4001

--- src/test/java/IgorKupchack/Bank/ClientTest.java
+++ src/test/java/IgorKupchack/Bank/ClientTest.java
@@ -2,14 +2,24 @@ package IgorKupchack.Bank;
 
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ClientTest {
-    @Test //TODO Task 5001
-    public void inheritanceTest()throws Exception{
+    @Test
+    public void inheritanceTest() throws Exception {
         Client cl = new Client();
         String testName = "Nazar";
         cl.setName(testName);
         assertEquals(testName, cl.getName());
+    }
+
+    @Test // TODO Task 4001
+    public void phoneNumberTest() throws Exception {
+        Client cl = new Client();
+        String testPhone1 = "0971073443";
+        String testPhone2 = "380971073443";
+        assertEquals("Incorect phone number format", cl.setPhone(testPhone1));
+        assertEquals("Phone number is setup", cl.setPhone(testPhone2));
     }
 }
